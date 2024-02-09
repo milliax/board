98e9e970be8c5d5ea703d1e280d7f8af466173a2
milliax <wei@milliax.me>
2024-02-09 15:53:30 +0000

    battery is dying

--- app/(application)/@tasks/page.tsx
+++ app/(application)/@tasks/page.tsx
@@ -1,7 +1,52 @@
+"use client"
+
+import clsx from "clsx";
+import { useState } from "react";
+
+import { SlOptionsVertical } from "react-icons/sl";
+import { MdDragIndicator } from "react-icons/md";
+
 export default function Tasks() {
     return (
-        <>
-            Task
-        </>
+        <div className="flex flex-col px-3 py-4 overflow-y-auto h-screen">
+            <Task title="寫網頁"
+                task={[
+                    { title: "排版", id: "jsefijsldkf" },
+                    { title: "美編", id: "jsejsldkf" },
+                ]}
+                color="purple"
+            />
+        </div>
+    )
+}
+
+const colorPreset = {
+    "purple": { header: "bg-purple-300 text-black", shadow: "hover:bg-purple-400" }
+}
+
+function Task({
+    title,
+    task,
+    color
+}: {
+    title: string,
+    task: { title: string, id: string }[],
+    color: 'purple'
+}) {
+    return (
+        <div className="bg-slate-200 rounded-md">
+            <div className={clsx(colorPreset[color]['header'], "h-10 px-3 items-center flex flex-row justify-between rounded-t-md font-semibold")}>
+                <h2>{title}</h2>
+                <div>
+                    <SlOptionsVertical className={clsx(colorPreset[color]["shadow"], "cursor-pointer rounded-full p-1 h-6 w-6 hover:shadow-lg")} />
+                </div>
+            </div>
+            {task.map(t => (
+                <div key={t.id} className="flex flex-row pl-5 items-center py-1 space-x-2">
+                    <MdDragIndicator className=""/>
+                    <h3 className="">{t.title}</h3>
+                </div>
+            ))}
+        </div>
     )
 }
--- app/(application)/page.tsx
+++ app/(application)/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import { IoSettingsOutline } from "react-icons/io5";
+
 
 export default function Home() {
+
   return (
-    <>
-      Page
-    </>
+    <div className="flex flex-col h-screen py-3 px-5">
+      <div className="h-8 bg-gray-100 px-3 flex flex-row items-center justify-between">
+        <h2>Febuary, 2024</h2>
+        <IoSettingsOutline />
+      </div>
+      Calendar
+    </div>
   );
 }
